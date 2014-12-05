6e6ad3eabb18c43634bebbb49a49d2b8bf267a14
Roman Dushko <rodush@gmail.com>
2014-12-05 10:11:37 +0200

    Initial commit - Rails App started

---
+++ README.md
@@ -0,0 +1,8 @@
+The Project helps to track auto and moto vehicles belonging to employees.
+This service may be useful if vehicles are parked tight and one blocking another to move away.
+You may quickly and easily find an owner of car by entering car registration number.
+
+TODO:
+- Send a SMS to blocking owner with system default or user-configured message, e.g.: 
+  "Please, unblock my car! I'm waiting near it".
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Autopark::Application.config.secret_key_base = '00acc6977f979889e77dc29b54d37c88ab3b959093bcc5c23ad5fe91af52cc556cb5837ac8e6bbb7ac794ee9037adb097531aa4cbd84786dfa618fa935e2b2da'
