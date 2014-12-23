4c5ca1e2a5199a25d0a97837bb3f8c6d3df1c6fd
Roman Dushko <rodush@gmail.com>
2014-12-23 19:12:34 +0200

    ADD: User model

---
+++ db/migrate/20141223171157_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string, :username
+      t.string, :phone
+      t.boolean :has_passcard
+
+      t.timestamps
+    end
+  end
+end
