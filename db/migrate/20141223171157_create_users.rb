00a9bb780204001af7e4a68609aadf223ecc24ca
Roman Dushko <rodush@gmail.com>
2014-12-23 21:41:09 +0200

    fix field names of user model

--- db/migrate/20141223171157_create_users.rb
+++ db/migrate/20141223171157_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string, :username
-      t.string, :phone
+      t.string :username
+      t.string :phone
       t.boolean :has_passcard
 
       t.timestamps
